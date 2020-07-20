9d10e1995e12655644eeb93ebeb319f9991d2ceb
Gradt <carolinefell@yandex.ru>
2020-07-20 10:06:50 +0300

    Corrected the code

--- src/components/main/TopSales.js
+++ src/components/main/TopSales.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchTopSalesRequest, fetchTopSalesClear } from '../../actions/topSalesActions';
-import Card from '../catalog/Product/Card';
+import Card from '../catalog/product/Card';
 
 // TopSales block
 
